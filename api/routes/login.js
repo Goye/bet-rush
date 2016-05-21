3aa2fb6759fb0dff6bb838baf9658f9e9348c831
Goye <karlos.goye@gmail.com>
2016-05-21 17:37:19 -0500

    facebook auth

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,4 +1,7 @@
 var controllers = require('../controllers');
+var passport = require('passport');
+var passportConfig = require('../config/passport');
 exports.init = function(app, auth) {
-    app.get('/', controllers.Login.doLogin);
+    app.get('/auth/facebook', passport.authenticate('facebook'));
+    app.get('/auth/facebook/callback', controllers.Login.facebookCallback);
 };
