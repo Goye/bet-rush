c72b687d3896abfd355983c01ecdc07b1f97b22e
Goye <karlos.goye@gmail.com>
2016-05-21 21:49:05 -0500

    delete files

--- bet_identifier/util/deleteEventData.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-import json
-
-client = MongoClient('localhost', 27017)
-db = client.betrush
-events = db.event
-events.remove()
--- bet_identifier/util/listEvents.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-import json
-
-client = MongoClient('localhost', 27017)
-db = client.betrush
-events = db.event
-for event in events.find():
-    print(event)
